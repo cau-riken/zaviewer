9cd4279005229838914fc7afb4d131466e0c9e3b
FPa-riken <frederic.papazian@riken.jp>
2020-05-29 10:16:45 +0200

    add targets in build script for production version

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,13 +1,16 @@
 
-var gulp = require("gulp");
+const gulp = require("gulp");
 
-var sass = require('gulp-sass');
-var browserify = require('browserify');
-var babelify = require('babelify');
+const sass = require('gulp-sass');
+const browserify = require('browserify');
+const babelify = require('babelify');
+const uglify = require('gulp-uglify');
+const stripCssComments = require('gulp-strip-css-comments');
 
-var concat = require("gulp-concat");
-var source = require('vinyl-source-stream');
-var buffer = require('vinyl-buffer');
+
+const concat = require("gulp-concat");
+const source = require('vinyl-source-stream');
+const    buffer = require('vinyl-buffer');
 
 
 gulp.task('sass', function () {
@@ -34,4 +37,40 @@ gulp.task('buildall', gulp.parallel('sass', 'build'), (done) => {
 
 gulp.task('watch', function () {
     gulp.watch('./src/js/**/*.js', gulp.series('buildall'));
+});
+
+
+gulp.task('apply-prod-environment', (done) => {
+    process.env.NODE_ENV = 'production';
+    done();
+});
+
+gulp.task('sassprod', function () {
+    return gulp.src('src/js/components/*.scss')
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(stripCssComments())
+        .pipe(concat("ZAViewer.css"))
+        .pipe(gulp.dest('./assets/css'));
+});
+
+gulp.task('buildprod', function () {
+    return browserify({ entries: './src/js/main.js', debug: true })
+        .transform("babelify", {
+            presets: ['@babel/preset-env', "@babel/preset-react"]
+        })
+        .bundle()
+        .pipe(source('main.js'))
+        .pipe(buffer())
+        .pipe(uglify({
+            mangle: false,
+            output: {
+                beautify: false,
+                comments: false
+            }
+        }))
+        .pipe(gulp.dest('./assets/js'));
+});
+
+gulp.task('buildprodall', gulp.series('apply-prod-environment', 'sassprod', 'buildprod'), (done) => {
+    done();
 });
