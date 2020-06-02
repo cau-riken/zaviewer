65ba1e3a0736a09a2c35ba9839bf36364272437f
FPa-riken <frederic.papazian@riken.jp>
2020-06-02 07:26:23 +0200

    revamp layers control panel

--- src/js/components/SliderNavigatorPanel.js
+++ src/js/components/SliderNavigatorPanel.js
@@ -1,6 +1,11 @@
 import React from 'react';
 
-import ExpandablePanel from './ExpandablePanel.js';
+import {
+    AnchorButton,
+    Icon,
+    Slider,
+    Switch
+} from "@blueprintjs/core";
 
 import ViewerManager from '../ViewerManager.js'
 
@@ -16,32 +21,38 @@ class LayerSlider extends React.Component {
         const { layerid, name, opacity, enabled } = this.props;
 
         return (
-            <div className="dataset">
-                <span id={layerid + "Name"} onChange={this.handleClickName.bind(this, layerid)}>{name}</span>
-                <br />
-                <div>
-                    <div>
-                        <input type="checkbox" id={layerid + "Enabled"} className="opcChk" checked={enabled}
-                            onChange={this.handleCheckedChange.bind(this, layerid, opacity)} />
-                    </div>
-                    <div>
-                        <input
-                            disabled={!enabled}
-                            type="range" id={layerid} className="slider" value={opacity}
-                            onChange={this.handleOpacityChange.bind(this, layerid, enabled)}
-                        />
-                    </div>
-                    <input
+            <div
+                style={{ width: 196, marginLeft: 10 }}
+            >
+                <div title="toggle layer's visibility">
+                    <Switch
+                        checked={enabled}
+                        onChange={this.handleCheckedChange.bind(this, layerid, opacity)}
+                        label={name}
+                    />
+                </div>
+
+                <div title="adjust layer's opacity">
+                    <Slider
+                        className="zav-Slider zav-OpacitySlider"
+                        min={0}
+                        max={100}
+                        stepSize={1}
+                        labelStepSize={100}
+                        onChange={this.handleOpacityChange.bind(this, layerid, enabled)}
+                        value={opacity}
+                        showTrackFill={false}
+                        labelRenderer={(value) => value + "%"}
                         disabled={!enabled}
-                        type="text" id={layerid + "Opacity"} className="opacity" value={opacity}
-                        onChange={this.handleOpacityChange.bind(this, layerid, enabled)} />
+                    />
                 </div>
             </div>
+
         );
     }
 
-    handleOpacityChange(layerid, enabled, event) {
-        ViewerManager.changeLayerOpacity(layerid, enabled, event.target.value);
+    handleOpacityChange(layerid, enabled, value) {
+        ViewerManager.changeLayerOpacity(layerid, enabled, value);
     }
 
     handleCheckedChange(layerid, opacity, event) {
@@ -66,18 +77,15 @@ class SliderNavigatorPanel extends React.Component {
         if (this.props.displaySettings) {
             $.each(this.props.displaySettings, function (layerid, value) {
                 var params = { layerid: layerid, name: value.name, opacity: value.opacity, enabled: value.enabled };
-                layerSliders.push(<LayerSlider key={"slid_" + layerid}  {...params} />)
+                layerSliders.push(<LayerSlider key={"slid_" + layerid}  {...params} />);
+                layerSliders.push(<div key={"sepslid_" + layerid} style={{ borderBottom: "dotted 1px #8a8a8a", marginBottom: 3 }} />);
             });
         }
         layerSliders.reverse();
 
         return (
             <React.Fragment>
-                <div className="sliderGroup">
-                    <div id="sliderGroup1">
-                        {layerSliders}
-                    </div>
-                </div>
+                {layerSliders.slice(1)}
             </React.Fragment>
 
         );
